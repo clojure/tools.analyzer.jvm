67b31bba4ef6452573aaaf5454bf7109422d0369
Alex Miller <alex@puredanger.com>
2013-10-04 11:38:03 -0500

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# tools.analyzer.jvm
+
+Additional jvm-specific passes for tools.analyzer
+
+## Releases and Dependency Information
+
+
+
+## License
+
+Copyright © 2013 Nicola Mometto, Rich Hickey & contributors.
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tools.analyzer "0.0.1-SNAPSHOT"
+  :description "Additional jvm-specific passes for tools.analyzer."
+  :url "https://github.com/clojure/tools.analyzer.jvm"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
