5b8910868ec8e36e0cd6bdda21557f4b5e0257c9
Bronsa <brobronsa@gmail.com>
2013-11-01 21:04:18 +0100

    whitespace fix

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :source-paths ["src/main/clojure"]
+  :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.analyzer "0.0.1-SNAPSHOT"]
