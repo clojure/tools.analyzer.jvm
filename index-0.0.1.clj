0f7e92ba227a9e58feeac08482a1f4416e3c3483
Tom Faulhaber <git_net@infolace.com>
2013-11-10 18:23:37 -0800

    Autodoc commit for master/7f0323cc

--- index-0.0.1.clj
+++ index-0.0.1.clj
@@ -5,4 +5,18 @@
    "http://clojure.github.com/tools.analyzer.jvm/clojure.tools.analyzer.jvm-api.html",
    :name "clojure.tools.analyzer.jvm",
    :doc nil}),
- :vars ()}
+ :vars
+ ({:arglists ([form env]),
+   :name "analyze",
+   :namespace "clojure.tools.analyzer.jvm",
+   :source-url
+   "https://github.com/clojure/tools.analyzer.jvm/blob/2346947d3e03b9b40dc1df49714f019f82ffe3df/src/main/clojure/clojure/tools/analyzer/jvm.clj#L277",
+   :raw-source-url
+   "https://github.com/clojure/tools.analyzer.jvm/raw/2346947d3e03b9b40dc1df49714f019f82ffe3df/src/main/clojure/clojure/tools/analyzer/jvm.clj",
+   :wiki-url
+   "http://clojure.github.com/tools.analyzer.jvm//clojure.tools.analyzer.jvm-api.html#clojure.tools.analyzer.jvm/analyze",
+   :doc
+   "Given an environment, a map containing\n  -  :locals (mapping of names to lexical bindings),\n  -  :context (one of :statement, :expr or :return\nand form, returns an expression object (a map containing at least :form, :op and :env keys).",
+   :var-type "function",
+   :line 277,
+   :file "src/main/clojure/clojure/tools/analyzer/jvm.clj"})}
