f18cb10f55a828eec1965bd952575c174389e543
Bronsa <brobronsa@gmail.com>
2013-11-22 02:49:09 +0100

    more tests

--- src/test/clojure/clojure/tools/analyzer/jvm/passes_test.clj
+++ src/test/clojure/clojure/tools/analyzer/jvm/passes_test.clj
@@ -3,13 +3,21 @@
   (:require [clojure.tools.analyzer.passes :refer :all]
             [clojure.test :refer [deftest is]]
             [clojure.set :as set]
+            [clojure.tools.analyzer.passes.add-binding-atom :refer [add-binding-atom]]
             [clojure.tools.analyzer.jvm.core-test :refer [ast e]]
             [clojure.tools.analyzer.passes.jvm.emit-form
              :refer [emit-form emit-hygienic-form]]
             [clojure.tools.analyzer.passes.jvm.validate :refer [validate]]
+            [clojure.tools.analyzer.passes.jvm.annotate-tag
+             :refer [annotate-literal-tag annotate-binding-tag]]
             [clojure.tools.analyzer.passes.jvm.clear-locals :refer [clear-locals]]
             [clojure.tools.analyzer.passes.jvm.annotate-branch :refer [annotate-branch]]
-            [clojure.tools.analyzer.passes.jvm.analyze-host-expr :refer [analyze-host-expr]]))
+            [clojure.tools.analyzer.passes.jvm.fix-case-test :refer [fix-case-test]]
+            [clojure.tools.analyzer.passes.jvm.analyze-host-expr :refer [analyze-host-expr]])
+  (:import (clojure.lang PersistentVector IPersistentMap
+                         IPersistentSet ISeq Keyword Var
+                         Symbol AFunction)
+           java.util.regex.Pattern))
 
 (deftest emit-form-test
   (is (= '(monitor-enter 1) (emit-form (ast (monitor-enter 1)))))
@@ -43,7 +51,6 @@
 (deftest clear-locals-test
   (let [f-expr (-> (ast (fn [x] (if x x x) x (if x (do x x) (if x x x))))
                  (prewalk annotate-branch)
-
                  clear-locals :methods first :body)]
     (is (= true (-> f-expr :statements first :then :to-clear? nil?)))
     (is (= true (-> f-expr :statements first :else :to-clear? nil?)))
@@ -52,3 +59,23 @@
     (is (= true (-> f-expr :ret :then :ret :to-clear?)))
     (is (= true (-> f-expr :ret :else :then :to-clear?)))
     (is (= true (-> f-expr :ret :else :else :to-clear?)))))
+
+(deftest fix-case-test-test
+  (let [c-ast (-> (ast (case 1 1 1)) add-binding-atom (prewalk fix-case-test))]
+    (is (= true (-> c-ast :body :ret :test :atom deref :case-test)))))
+
+(deftest annotate-literal-tag-test
+  (is (= PersistentVector (-> {:op :const :form []} annotate-literal-tag :tag)))
+  (is (= PersistentVector (-> (ast []) annotate-literal-tag :tag)))
+  (is (= PersistentVector (-> (ast '[]) annotate-literal-tag :tag)))
+  (is (= IPersistentMap (-> (ast {}) annotate-literal-tag :tag)))
+  (is (= IPersistentSet (-> (ast #{}) annotate-literal-tag :tag)))
+  (is (= ISeq (-> (ast '()) annotate-literal-tag :tag)))
+  (is (= Class (-> {:op :const :type :class :form Object} annotate-literal-tag :tag)))
+  (is (= String (-> (ast "foo") annotate-literal-tag :tag)))
+  (is (= Keyword (-> (ast :foo) annotate-literal-tag :tag)))
+  (is (= Character/TYPE (-> (ast \f) annotate-literal-tag :tag)))
+  (is (= Long/TYPE (-> (ast 1) annotate-literal-tag :tag)))
+  (is (= Pattern (-> (ast #"foo") annotate-literal-tag :tag)))
+  (is (= Var (-> (ast #'+)  annotate-literal-tag :tag)))
+  (is (= Boolean (-> (ast true) annotate-literal-tag :tag))))
