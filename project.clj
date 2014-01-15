64ecab702a3a666f0a14562f0fa4c5339267bd07
Nicola Mometto <brobronsa@gmail.com>
2014-01-15 11:56:29 +0100

    TANAL-56: update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.analyzer.jvm "0.0.1-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.1.0-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
