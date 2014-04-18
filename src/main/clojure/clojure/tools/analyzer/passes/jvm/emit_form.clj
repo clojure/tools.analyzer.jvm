ca84b57bad47c2b38d500561800e3d44c26a685d
Nicola Mometto <brobronsa@gmail.com>
2014-04-18 20:37:40 +0200

    add :qualified-vars option to emit-form

--- src/main/clojure/clojure/tools/analyzer/passes/jvm/emit_form.clj
+++ src/main/clojure/clojure/tools/analyzer/passes/jvm/emit_form.clj
@@ -12,8 +12,8 @@
 (defmulti -emit-form (fn [{:keys [op]} _] op))
 
 (defn -emit-form*
-  [{:keys [form] :as ast} hygienic?]
-  (let [expr (-emit-form ast hygienic?)]
+  [{:keys [form] :as ast} ops]
+  (let [expr (-emit-form ast ops)]
     (if-let [m (and (instance? clojure.lang.IObj expr)
                     (meta form))]
       (with-meta expr (merge (meta expr) m))
@@ -21,111 +21,119 @@
 
 (defn emit-form
   "Return the form represented by the given AST"
-  [ast]
-  (binding [default/-emit-form* -emit-form*]
-    (-emit-form* ast false)))
+  ([ast] (emit-form ast #{}))
+  ([ast ops]
+     (binding [default/-emit-form* -emit-form*]
+       (-emit-form* ast ops))))
 
 (defn emit-hygienic-form
   "Return an hygienic form represented by the given AST"
   [ast]
   (binding [default/-emit-form* -emit-form*]
-    (-emit-form* ast true)))
+    (-emit-form* ast #{:hygienic})))
 
 (defmethod -emit-form :default
-  [ast hygienic?]
-  (default/-emit-form ast hygienic?))
+  [ast ops]
+  (default/-emit-form ast ops))
 
 (defmethod -emit-form :monitor-enter
-  [{:keys [target]} hygienic?]
-  `(monitor-enter ~(-emit-form* target hygienic?)))
+  [{:keys [target]} ops]
+  `(monitor-enter ~(-emit-form* target ops)))
 
 (defmethod -emit-form :monitor-exit
-  [{:keys [target]} hygienic?]
-  `(monitor-exit ~(-emit-form* target hygienic?)))
+  [{:keys [target]} ops]
+  `(monitor-exit ~(-emit-form* target ops)))
 
 (defmethod -emit-form :import
-  [{:keys [class]} hygienic?]
+  [{:keys [class]} ops]
   `(clojure.core/import* ~class))
 
 (defmethod -emit-form :the-var
-  [{:keys [^clojure.lang.Var var]} hygienic?]
+  [{:keys [^clojure.lang.Var var]} ops]
   `(var ~(symbol (name (ns-name (.ns var))) (name (.sym var)))))
 
 (defmethod -emit-form :method
-  [{:keys [params body this name form]} hygienic?]
+  [{:keys [params body this name form]} ops]
   (let [params (into [this] params)]
     `(~(with-meta name (meta (first form)))
-      ~(with-meta (mapv #(-emit-form* % hygienic?) params)
+      ~(with-meta (mapv #(-emit-form* % ops) params)
          (meta (second form)))
-      ~(-emit-form* body hygienic?))))
+      ~(-emit-form* body ops))))
 
 (defn class->sym [class]
   (symbol (.getName ^Class class)))
 
 (defmethod -emit-form :catch
-  [{:keys [class local body]} hygienic?]
-  `(catch ~(class->sym class) ~(-emit-form* local hygienic?)
-     ~(-emit-form* body hygienic?)))
+  [{:keys [class local body]} ops]
+  `(catch ~(class->sym class) ~(-emit-form* local ops)
+     ~(-emit-form* body ops)))
 
 (defmethod -emit-form :deftype
-  [{:keys [name class-name fields interfaces methods]} hygienic?]
-  `(deftype* ~name ~(class->sym class-name) ~(mapv #(-emit-form* % hygienic?) fields)
+  [{:keys [name class-name fields interfaces methods]} ops]
+  `(deftype* ~name ~(class->sym class-name) ~(mapv #(-emit-form* % ops) fields)
      :implements ~(mapv class->sym interfaces)
-     ~@(mapv #(-emit-form* % hygienic?) methods)))
+     ~@(mapv #(-emit-form* % ops) methods)))
 
 (defmethod -emit-form :reify
-  [{:keys [interfaces methods]} hygienic?]
+  [{:keys [interfaces methods]} ops]
   `(reify* ~(mapv class->sym (disj interfaces clojure.lang.IObj))
-           ~@(mapv #(-emit-form* % hygienic?) methods)))
+           ~@(mapv #(-emit-form* % ops) methods)))
 
 (defmethod -emit-form :case
-  [{:keys [test default tests thens shift mask low high switch-type test-type skip-check?]} hygienic?]
-  `(case* ~(-emit-form* test hygienic?)
+  [{:keys [test default tests thens shift mask low high switch-type test-type skip-check?]} ops]
+  `(case* ~(-emit-form* test ops)
           ~shift ~mask
-          ~(-emit-form* default hygienic?)
+          ~(-emit-form* default ops)
           ~(apply sorted-map
                   (mapcat (fn [{:keys [hash test]} {:keys [then]}]
-                            [hash [(-emit-form* test hygienic?) (-emit-form* then hygienic?)]])
+                            [hash [(-emit-form* test ops) (-emit-form* then ops)]])
                           tests thens))
           ~switch-type ~test-type ~skip-check?))
 
 (defmethod -emit-form :static-field
-  [{:keys [class field]} hygienic?]
+  [{:keys [class field]} ops]
   (symbol (.getName ^Class class) (name field)))
 
 (defmethod -emit-form :static-call
-  [{:keys [class method args]} hygienic?]
+  [{:keys [class method args]} ops]
   `(~(symbol (.getName ^Class class) (name method))
-    ~@(mapv #(-emit-form* % hygienic?) args)))
+    ~@(mapv #(-emit-form* % ops) args)))
 
 (defmethod -emit-form :instance-field
-  [{:keys [instance field]} hygienic?]
-  `(~(symbol (str ".-" (name field))) ~(-emit-form* instance hygienic?)))
+  [{:keys [instance field]} ops]
+  `(~(symbol (str ".-" (name field))) ~(-emit-form* instance ops)))
 
 (defmethod -emit-form :instance-call
-  [{:keys [instance method args]} hygienic?]
-  `(~(symbol (str "." (name method))) ~(-emit-form* instance hygienic?)
-    ~@(mapv #(-emit-form* % hygienic?) args)))
+  [{:keys [instance method args]} ops]
+  `(~(symbol (str "." (name method))) ~(-emit-form* instance ops)
+    ~@(mapv #(-emit-form* % ops) args)))
 
 (defmethod -emit-form :host-interop
-  [{:keys [target m-or-f]} hygienic?]
-  `(~(symbol (str "." (name m-or-f))) ~(-emit-form* target hygienic?)))
+  [{:keys [target m-or-f]} ops]
+  `(~(symbol (str "." (name m-or-f))) ~(-emit-form* target ops)))
 
 (defmethod -emit-form :prim-invoke
-  [{:keys [fn args]} hygienic?]
-  `(~(-emit-form* fn hygienic?)
-    ~@(mapv #(-emit-form* % hygienic?) args)))
+  [{:keys [fn args]} ops]
+  `(~(-emit-form* fn ops)
+    ~@(mapv #(-emit-form* % ops) args)))
 
 (defmethod -emit-form :protocol-invoke
-  [{:keys [fn args]} hygienic?]
-  `(~(-emit-form* fn hygienic?)
-    ~@(mapv #(-emit-form* % hygienic?) args)))
+  [{:keys [fn args]} ops]
+  `(~(-emit-form* fn ops)
+    ~@(mapv #(-emit-form* % ops) args)))
 
 (defmethod -emit-form :keyword-invoke
-  [{:keys [fn args]} hygienic?]
-  `(~(-emit-form* fn hygienic?)
-    ~@(mapv #(-emit-form* % hygienic?) args)))
+  [{:keys [fn args]} ops]
+  `(~(-emit-form* fn ops)
+    ~@(mapv #(-emit-form* % ops) args)))
 
 (defmethod -emit-form :instance?
-  [{:keys [class target]} hygienic?]
-  `(instance? ~class ~(-emit-form* target hygienic?)))
+  [{:keys [class target]} ops]
+  `(instance? ~class ~(-emit-form* target ops)))
+
+(defmethod -emit-form :var
+  [{:keys [form ^clojure.lang.Var var]} ops]
+  (if (:qualified-vars ops)
+    (with-meta (symbol (ns-name (.ns var)) (.sym var))
+      (meta form))
+    form))
