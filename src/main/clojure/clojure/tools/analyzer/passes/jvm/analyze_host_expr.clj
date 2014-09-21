clojure/tools.analyzer.jvm

commit e0b686a33e89d7d74d5131e3f4356a0a9aea20ff

workaround for TANAL-88, make class literals shadow Vars

src/main/clojure/clojure/tools/analyzer/passes/jvm/analyze_host_expr.clj
@@ -142,12 +142,15 @@
 (defn analyze-host-expr
   "Performing some reflection, transforms :host-interop/:host-call/:host-field
    nodes in either: :static-field, :static-call, :instance-call, :instance-field
-   or :host-interop nodes.
+   or :host-interop nodes, and a :var or :maybe-class node in a :const :class node,
+   if necessary (class literals shadow Vars).
 
-   A :host-interop node represents either an instance-field or a no-arg instance-method."
+   A :host-interop node represents either an instance-field or a no-arg instance-method.
+
+   "
   {:pass-info {:walk :post :depends #{}}}
   [{:keys [op target form tag env class] :as ast}]
-  (if (#{:host-interop :host-call :host-field :maybe-class} op)
+  (if (#{:host-interop :host-call :host-field :maybe-class :var} op)
     (let [class? (and (= :const (:op target))
                       (= :class (:type target))
                       (:form target))
@@ -170,6 +173,14 @@
                    :tag   Class
                    :o-tag Class
                    :form  form))
+
+               :var
+               (when-let [the-class (maybe-class form)]
+                 (assoc (ana/-analyze :const the-class env :class)
+                   :tag   Class
+                   :o-tag Class
+                   :form  form))
+
                (-analyze-host-expr target-type (:m-or-f ast)
                                    target class? env))
              (when tag

src/test/clojure/clojure/tools/analyzer/jvm/core_test.clj
@@ -90,3 +90,6 @@
     (is (= :loop (:op tree)))
     (is (.startsWith (name (:name chunk)) "chunk"))
     (is (= clojure.lang.IChunk (:tag chunk)))))
+
+(deftest analyze-proxy
+  (is (ast1 (proxy [Object] []))))

commit e54155a3c7aeacdcd7321ff57ddba4f0788161b0

TANAL-89 fix regression introduced with commit e0b686a3

src/main/clojure/clojure/tools/analyzer/passes/jvm/analyze_host_expr.clj
@@ -173,11 +173,12 @@
                    :form  form))
 
                :var
-               (when-let [the-class (maybe-class form)]
+               (if-let [the-class (maybe-class form)]
                  (assoc (ana/-analyze :const the-class env :class)
                    :tag   Class
                    :o-tag Class
-                   :form  form))
+                   :form  form)
+                 ast)
 
                (-analyze-host-expr target-type (:m-or-f ast)
                                    target class? env))

src/test/clojure/clojure/tools/analyzer/jvm/core_test.clj
@@ -91,5 +91,9 @@
     (is (.startsWith (name (:name chunk)) "chunk"))
     (is (= clojure.lang.IChunk (:tag chunk)))))
 
+(def ^:dynamic x)
+(deftest set!-dynamic-var
+  (is (ast1 (set! x 1))))
+
 (deftest analyze-proxy
   (is (ast1 (proxy [Object] []))))