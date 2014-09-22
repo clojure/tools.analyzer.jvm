09f7d0b4fc864c6e53befe4b5d97878d25b02ae9
Tom Faulhaber <git_net@infolace.com>
2014-09-22 09:17:33 -0700

    Autodoc commit for master/86e46d36

--- index-0.6.1.clj
+++ index-0.6.1.clj
@@ -97,7 +97,7 @@
    :name "clojure.tools.analyzer.passes.jvm.infer-tag",
    :doc nil}
   {:source-url
-   "https://github.com/clojure/tools.analyzer.jvm/blob/f82c2964746356bb9a6ca50f6502339215bbebbb/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj",
+   "https://github.com/clojure/tools.analyzer.jvm/blob/86e46d36a2a5a5e88100e8eaaaa47a18a2f34b5b/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj",
    :wiki-url
    "http://clojure.github.com/tools.analyzer.jvm/passes.jvm.validate-api.html",
    :name "clojure.tools.analyzer.passes.jvm.validate",
@@ -553,15 +553,15 @@
    :name "validate",
    :namespace "clojure.tools.analyzer.passes.jvm.validate",
    :source-url
-   "https://github.com/clojure/tools.analyzer.jvm/blob/f82c2964746356bb9a6ca50f6502339215bbebbb/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj#L227",
+   "https://github.com/clojure/tools.analyzer.jvm/blob/86e46d36a2a5a5e88100e8eaaaa47a18a2f34b5b/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj#L228",
    :raw-source-url
-   "https://github.com/clojure/tools.analyzer.jvm/raw/f82c2964746356bb9a6ca50f6502339215bbebbb/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj",
+   "https://github.com/clojure/tools.analyzer.jvm/raw/86e46d36a2a5a5e88100e8eaaaa47a18a2f34b5b/src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj",
    :wiki-url
    "http://clojure.github.com/tools.analyzer.jvm//passes.jvm.validate-api.html#clojure.tools.analyzer.passes.jvm.validate/validate",
    :doc
    "Validate tags, classes, method calls.\nThrows exceptions when invalid forms are encountered, replaces\nclass symbols with class objects.\n\nPasses opts:\n* :validate/throw-on-arity-mismatch\n   If true, validate will throw on potential arity mismatch\n* :validate/wrong-tag-handler\n   If bound to a function, will invoke that function instead of\n   throwing on invalid tag.\n   The function takes the tag key and the AST and must return\n   a map of tag key -> valid tag value (or nil)\n* :validate/unresolvable-symbol-handler\n   If bound to a function, will invoke that function instead of\n   throwing on unresolvable symbol.\n   The function takes three arguments: the namespace (possibly nil)\n   and name part of the symbol, as symbols and the originating\n   AST node which can be either a :maybe-class or a :maybe-host-form,\n   those nodes are documented in the tools.analyzer quickref.\n   The function must return a valid tools.analyzer.jvm AST node.",
    :var-type "function",
-   :line 227,
+   :line 228,
    :file
    "src/main/clojure/clojure/tools/analyzer/passes/jvm/validate.clj"}
   {:arglists ([analyze]),
