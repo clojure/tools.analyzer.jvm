4c4267e2b1d6d4d9c4b44428e7f4dd4acd43c109
Nicola Mometto <brobronsa@gmail.com>
2014-09-24 11:06:39 +0200

    bump clojure dep in project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/core.memoize "0.5.6"]
                  [org.clojure/tools.reader "0.8.8"]
                  [org.clojure/tools.analyzer "0.6.1-SNAPSHOT"]
