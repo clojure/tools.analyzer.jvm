7998e2f3827987b62d52076fcbbd7bbd8f1702c4
Nicola Mometto <brobronsa@gmail.com>
2014-10-31 02:19:12 +0100

    bump tools.reader dep

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [org.clojure/tools.reader "0.8.10"]
+                 [org.clojure/tools.reader "0.8.11"]
                  [org.clojure/tools.analyzer "0.6.3-SNAPSHOT"]
                  [org.ow2.asm/asm-all "4.2"]])
