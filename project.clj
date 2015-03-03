403e51a7930f2a9771aff80ec7d16296f73704df
Nicola Mometto <brobronsa@gmail.com>
2015-03-03 01:15:54 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojure/tools.analyzer.jvm "0.6.6-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.6.7-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [org.clojure/tools.reader "0.8.13"]
-                 [org.clojure/tools.analyzer "0.6.5-SNAPSHOT"]
+                 [org.clojure/tools.reader "0.8.15"]
+                 [org.clojure/tools.analyzer "0.6.6-SNAPSHOT"]
                  [org.ow2.asm/asm-all "4.2"]])
