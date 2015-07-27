966fe1ff985039bf29d640fbd8bf95ea50aad237
Nicola Mometto <brobronsa@gmail.com>
2015-07-27 13:12:58 +0200

    TANAL-112: avoid reflection on catch local

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]
                  [org.clojure/core.memoize "0.5.6"]
                  [org.clojure/tools.reader "0.9.2"]
                  [org.clojure/tools.analyzer "0.6.7-SNAPSHOT"]
