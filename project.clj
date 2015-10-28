aa41aa99cbfe86c0ef5bce49e7be7ab694756573
Nicola Mometto <brobronsa@gmail.com>
2015-10-28 21:37:52 +0000

    bump core.memoize version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]
-                 [org.clojure/core.memoize "0.5.6"]
+                 [org.clojure/core.memoize "0.5.7"]
                  [org.clojure/tools.reader "0.10.0"]
                  [org.clojure/tools.analyzer "0.6.7-SNAPSHOT"]
                  [org.ow2.asm/asm-all "4.2"]])
