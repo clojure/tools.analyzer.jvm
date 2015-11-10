295b0756ede60ddb555ea3c3b7f8c3eb54b2c5af
Nicola Mometto <brobronsa@gmail.com>
2015-11-10 13:49:11 +0000

    update deps

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]
-                 [org.clojure/core.memoize "0.5.7"]
+                 [org.clojure/core.memoize "0.5.8"]
                  [org.clojure/tools.reader "1.0.0-alpha1"]
                  [org.clojure/tools.analyzer "0.6.8-SNAPSHOT"]
                  [org.ow2.asm/asm-all "4.2"]])
