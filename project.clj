6e07db2e0efbafcab813ae4820d515f29277db9c
Nicola Mometto <brobronsa@gmail.com>
2016-06-17 16:14:27 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]
-                 [org.clojure/core.memoize "0.5.8"]
-                 [org.clojure/tools.reader "1.0.0-alpha1"]
-                 [org.clojure/tools.analyzer "0.6.8-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+                 [org.clojure/core.memoize "0.5.9"]
+                 [org.clojure/tools.reader "1.0.0-beta2"]
+                 [org.clojure/tools.analyzer "0.6.9"]
                  [org.ow2.asm/asm-all "4.2"]])
