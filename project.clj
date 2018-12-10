745032002680d203f03f9875b8296a96d938e16d
Nicola Mometto <me@bronsa.net>
2018-12-10 13:59:58 +0000

    bump deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.analyzer.jvm "0.7.1-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.7.3-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
@@ -7,8 +7,8 @@
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.memoize "0.5.9"]
-                 [org.clojure/tools.reader "1.1.1"]
-                 [org.clojure/tools.analyzer "0.6.9"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [org.clojure/tools.analyzer "0.7.0"]
                  [org.ow2.asm/asm "5.2"]]
   :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/releases"]
                  ["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
