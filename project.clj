315403450d13073019e968a927e558d41c15d390
Alex Miller <alex.miller@cognitect.com>
2024-02-19 15:52:04 -0600

    update deps and parent pom

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.memoize "1.0.253"]
-                 [org.clojure/tools.reader "1.3.6"]
-                 [org.clojure/tools.analyzer "1.1.1"]
+                 [org.clojure/core.memoize "1.1.266"]
+                 [org.clojure/tools.reader "1.4.0"]
+                 [org.clojure/tools.analyzer "1.2.0"]
                  [org.ow2.asm/asm "9.2"]]
   :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/releases"]
                  ["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
